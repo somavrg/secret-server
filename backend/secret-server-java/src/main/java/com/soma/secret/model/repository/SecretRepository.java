88a7180d66e4135e00b8bda7ff668d2dbfcdbf03
somavrg <varga.j.soma@gmail.com>
2024-01-23 12:31:05 +0100

    feat: fix SecretRepository interface

--- backend/secret-server-java/src/main/java/com/soma/secret/model/repository/SecretRepository.java
+++ backend/secret-server-java/src/main/java/com/soma/secret/model/repository/SecretRepository.java
@@ -3,8 +3,12 @@ package com.soma.secret.model.repository;
 import com.soma.secret.model.Secret;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SecretRepository extends JpaRepository<Secret, String> {
-    Secret findSecretByHash(UUID hash);
+    Optional<Secret> findSecretByHashAndRemainingViewsGreaterThanAndExpiresAtAfter(UUID hash,
+                                                                                   int limit,
+                                                                                   LocalDateTime dateTime);
 }
