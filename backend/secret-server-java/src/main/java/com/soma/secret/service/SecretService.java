4dd447ba20d306c2d188da256a27f28e671ebf2f
somavrg <varga.j.soma@gmail.com>
2024-01-23 12:30:45 +0100

    feat: fix getSecretsByHash method

--- backend/secret-server-java/src/main/java/com/soma/secret/service/SecretService.java
+++ backend/secret-server-java/src/main/java/com/soma/secret/service/SecretService.java
@@ -8,5 +8,5 @@ import java.util.UUID;
 
 public interface SecretService {
     SecretDTO addSecret(NewSecretDTO newSecretDTO);
-    Optional<SecretDTO> getSecretByHash(UUID hash);
+    SecretDTO getSecretByHash(UUID hash);
 }
