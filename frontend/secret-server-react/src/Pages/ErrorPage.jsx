1c9bcf86db4ab43311d3d627dff7c151f671c3cc
somavrg <varga.j.soma@gmail.com>
2024-01-24 14:01:39 +0100

    feat: create ErrorPage
    (cherry picked from commit 619a46ca639414461030cabd1134fe9e16d867a6)

---
+++ frontend/secret-server-react/src/Pages/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
