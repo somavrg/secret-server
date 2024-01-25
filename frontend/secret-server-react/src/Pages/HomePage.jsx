5ffb41dd65a0319313a9bb1f6b891c645a09f217
somavrg <varga.j.soma@gmail.com>
2024-01-25 12:03:58 +0100

    feat: redo HomePage with Bootstrap

--- frontend/secret-server-react/src/Pages/HomePage.jsx
+++ frontend/secret-server-react/src/Pages/HomePage.jsx
@@ -1,17 +1,18 @@
 import {useNavigate} from "react-router-dom";
-
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomePage = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="buttons">
-            <button onClick={() => navigate("/create")}>
+        <div className="container d-flex justify-content-center align-items-center p-3 m-5">
+            <Button onClick={() => navigate("/create")} className="btn-lg me-4 btn-outline-primary btn-light">
                 Add new Secret
-            </button>
-            <button onClick={() => navigate("/retrive")}>
+            </Button>
+            <Button onClick={() => navigate("/retrieve")} className="btn-lg btn-outline-primary btn-light">
                 Search a Secret
-            </button>
+            </Button>
         </div>
     )
 }
