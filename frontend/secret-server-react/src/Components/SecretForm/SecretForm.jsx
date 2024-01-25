17d3190adbb170ca52c442ff9e79a28b837132d4
somavrg <varga.j.soma@gmail.com>
2024-01-25 12:04:37 +0100

    feat: redo SecretForm Component with Bootstrap

--- frontend/secret-server-react/src/Components/SecretForm/SecretForm.jsx
+++ frontend/secret-server-react/src/Components/SecretForm/SecretForm.jsx
@@ -1,6 +1,12 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FormLabel} from "react-bootstrap";
+
+
 const SecretForm = ({secret, onSave, onCancel}) => {
 
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
@@ -18,40 +24,51 @@ const SecretForm = ({secret, onSave, onCancel}) => {
 
 
     return (
-        <form className="SecretForm" onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="secretText">Secret Text:</label>
-                <input
-                    value={secret?.secretText}
-                    name="secretText"
-                    id="secretText"
-                />
-            </div>
-            <div>
-                <label htmlFor="expireAfterViews">Times it can be viewed:</label>
-                <input
-                    value={secret?.expireAfterViews}
-                    name="expireAfterViews"
-                    id="expireAfterViews"
-                />
-            </div>
-            <div>
-                <label htmlFor="expireAfter">Minutes it can be viewed:</label>
-                <input
-                    value={secret?.expireAfter}
-                    name="expireAfter"
-                    id="expireAfter"
-                />
-            </div>
-            <div className="buttons">
-                <button type="submit">
-                    Create New Secret
-                </button>
-                <button type="button" onClick={onCancel}>
-                    Cancel
-                </button>
-            </div>
-        </form>
+        <div className="container d-flex justify-content-center align-items-center">
+            <Form onSubmit={handleSubmit} className="d-inline-block p-3">
+                <FormLabel className="justify-content-center fs-3 mb-3 fw-bold m-3">Add New Secret</FormLabel>
+                <Form.Group>
+                    <Form.Label>Secret Text:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={secret?.secretText}
+                        name="secretText"
+                        id="secretText"
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Times it can be viewed:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={secret?.expireAfterViews}
+                        name="expireAfterViews"
+                        id="expireAfterViews"
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Minutes it can be viewed:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={secret?.expireAfter}
+                        name="expireAfter"
+                        id="expireAfter"
+                        className="mb-3"
+                    />
+                </Form.Group>
+
+                <div className="buttons">
+                    <Button variant="primary" type="submit" className="btn-sm me-2">
+                        Create New Secret
+                    </Button>
+                    <Button variant="secondary" onClick={onCancel} className="btn-sm">
+                        Cancel
+                    </Button>
+                </div>
+            </Form>
+        </div>
+
     );
 
 };
