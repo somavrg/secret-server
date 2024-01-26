c5ffea19fbb4f544e4a7b8213006390d2b2a9cfd
somavrg <varga.j.soma@gmail.com>
2024-01-26 12:29:01 +0100

    feat: annotate fields

--- backend/secret-server-java/src/main/java/com/soma/secret/model/DTO/NewSecretDTO.java
+++ backend/secret-server-java/src/main/java/com/soma/secret/model/DTO/NewSecretDTO.java
@@ -1,9 +1,14 @@
 package com.soma.secret.model.DTO;
 
 
+import lombok.NonNull;
+
 public record NewSecretDTO(
+        @NonNull
         String secretText,
+        @NonNull
         int expireAfterViews,
+        @NonNull
         int expireAfter
 ) {
 }
