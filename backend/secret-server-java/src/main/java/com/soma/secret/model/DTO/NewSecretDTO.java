528a228e589aba2d53c0402c23fb95bf4c8498a7
somavrg <varga.j.soma@gmail.com>
2024-01-26 12:33:30 +0100

    fix: annotate delete

--- backend/secret-server-java/src/main/java/com/soma/secret/model/DTO/NewSecretDTO.java
+++ backend/secret-server-java/src/main/java/com/soma/secret/model/DTO/NewSecretDTO.java
@@ -1,14 +1,8 @@
 package com.soma.secret.model.DTO;
 
-
-import lombok.NonNull;
-
 public record NewSecretDTO(
-        @NonNull
         String secretText,
-        @NonNull
         int expireAfterViews,
-        @NonNull
         int expireAfter
 ) {
 }
